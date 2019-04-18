619bed4ac4ca58966285a81b1df1c07ec31fa173
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-04-18 14:11:14 +0400

    small fixes

--- builder/scripts/300_back.sh
+++ builder/scripts/300_back.sh
@@ -1,8 +1,5 @@
 git clone $4 $1/back && \
-#mv $1/start.sh $1/back/rundoctrine.sh  && \
 cd $1/back && \
 chmod 755 .env && \
 chmod 755 start.sh && \
-#ls -al &&\
-composer install --no-plugins --no-scripts #&& \
-#ls -al
+composer install --no-plugins --no-scripts

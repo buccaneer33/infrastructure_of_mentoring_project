8f76449911096fd8600ffe61fd1fbab9fd16de7d
Evgenii Kozlov1 <Evgenii_Kozlov1@epam.com>
2019-04-18 13:35:06 +0400

    done

--- builder/scripts/300_back.sh
+++ builder/scripts/300_back.sh
@@ -2,6 +2,7 @@ git clone $4 $1/back && \
 #mv $1/start.sh $1/back/rundoctrine.sh  && \
 cd $1/back && \
 chmod 755 .env && \
-#chmod 444 rundoctrine.sh && \
-#composer install --no-plugins --no-scripts && \
-ls -alF
+chmod 755 start.sh && \
+#ls -al &&\
+composer install --no-plugins --no-scripts #&& \
+#ls -al
